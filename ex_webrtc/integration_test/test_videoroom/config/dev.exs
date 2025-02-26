343a1547078a53ee542c8c4f631936c6def8053c
Tomasz Mazur <47872060+AHGIJMKLKKZNPJKQR@users.noreply.github.com>
2025-02-26 17:38:08 +0100

    Remove debug logs

--- ex_webrtc/integration_test/test_videoroom/config/dev.exs
+++ ex_webrtc/integration_test/test_videoroom/config/dev.exs
@@ -8,6 +8,6 @@ config :test_videoroom, TestVideoroomWeb.Endpoint,
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
 
-config :logger, :console, format: "[$level] $message\n", truncate: :infinity, level: :debug
+config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 config :phoenix, :plug_init_mode, :runtime
