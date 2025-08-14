0f10c98ceb42e639413523df8e8e0349ff9a243f
Przemysław Rożnawski <roznawski.przemyslaw@gmail.com>
2025-08-14 15:06:03 +0200

    Add agent to CI

--- transcoder/test/test_helper.exs
+++
@@ -1,4 +0,0 @@
-Code.require_file("test/support/test_source.ex", "../engine")
-Code.require_file("test/support/fake_source_endpoint.ex", "../engine")
-
-ExUnit.start(capture_log: true, exclude: [:skip])
