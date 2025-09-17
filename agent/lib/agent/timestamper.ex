56cbe17780dd4f56f4dfcc0ed0c96233954acc4d
Tomasz Mazur <47872060+AHGIJMKLKKZNPJKQR@users.noreply.github.com>
2025-09-17 13:07:50 +0200

    Use reset event instead of increasing pts

--- agent/lib/agent/timestamper.ex
+++ agent/lib/agent/timestamper.ex
@@ -38,25 +38,25 @@ defmodule Membrane.RTC.Engine.Endpoint.Agent.Timestamper do
   @impl true
   def handle_buffer(_pad, buffer, ctx, state) do
     stream_format = get_in(ctx, [:pads, :output, :stream_format])
-    state = maybe_reset_pts(state)
+
+    {actions, state} = maybe_reset_pts(state)
     buffer = %Membrane.Buffer{buffer | pts: state.next_pts}
 
-    {[buffer: {:output, buffer}], update_next_pts(buffer, stream_format, state)}
+    {actions ++ [buffer: {:output, buffer}], update_next_pts(buffer, stream_format, state)}
+  end
+
+  defp maybe_reset_pts(%{start_ts: nil} = state) do
+    {[], %{state | start_ts: Membrane.Time.os_time()}}
   end
 
   defp maybe_reset_pts(%{next_pts: next_pts, start_ts: start_ts} = state) do
-    now_ts = Membrane.Time.os_time()
-    start_ts = start_ts || now_ts
-    arrival_time = now_ts - start_ts
+    arrival_time = Membrane.Time.os_time() - start_ts
 
-    next_pts =
-      if arrival_time > next_pts + @max_jitter_duration do
-        arrival_time
-      else
-        next_pts
-      end
-
-    %{state | start_ts: start_ts, next_pts: next_pts}
+    if arrival_time > next_pts + @max_jitter_duration do
+      {[event: {:output, %Membrane.Realtimer.Events.Reset{}}], state}
+    else
+      {[], state}
+    end
   end
 
   defp update_next_pts(
